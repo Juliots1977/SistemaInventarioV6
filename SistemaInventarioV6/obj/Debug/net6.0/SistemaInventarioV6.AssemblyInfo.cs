5cdd8b2999978e41212a4240479ac468659ebf25
juliots1977 <juliots@gmail.com>
2024-02-29 17:15:53 +0100

    Configuracion del Proyecto

--- SistemaInventarioV6/obj/Debug/net6.0/SistemaInventarioV6.AssemblyInfo.cs
+++ SistemaInventarioV6/obj/Debug/net6.0/SistemaInventarioV6.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventarioV6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f63f639998d3b1819e30a47a289c361c7b2bb649")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventarioV6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventarioV6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
